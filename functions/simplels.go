11329f33a72ab811e0702788bc5fbfe72a36ba71
Maxwell Lysle <max@lysle.org>
2026-02-25 12:42:57 -0700

    partition files and dirs

--- functions/simplels.go
+++ functions/simplels.go
@@ -1,9 +1,31 @@
 package functions
 
 import (
+	"fmt"
 	"io"
+	"os"
+	"sort"
 )
 
 func SimpleLS(w io.Writer, args []string, useColor bool) {
+	files, dirs := Partition(args)
+}
 
+func Partition(args []string) (files []string, dirs []string) {
+	files = []string{}
+	dirs = []string{}
+	for _, arg := range args {
+		info, err := os.Lstat(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fprintf: %v\n", err)
+		}
+		if info.IsDir() {
+			dirs = append(dirs, arg)
+		} else {
+			files = append(files, arg)
+		}
+	}
+	sort.Strings(files)
+	sort.Strings(dirs)
+	return files, dirs
 }
