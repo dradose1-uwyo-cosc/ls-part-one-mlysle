262c642982a704484e92ddf1d7e269d68371a658
Maxwell Lysle <max@lysle.org>
2026-02-25 12:44:03 -0700

    setup call to simpleLS from main

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
-package gols
+package main
 
 import (
+	"flag" // for flag.Arg
 	"gols/functions"
+	"io"
+	"os"
 )
+
+func main() {
+	var writer io.Writer
+	args := flag.Args()
+	useColor := functions.IsTerminal(os.Stdout)
+
+	functions.SimpleLS(writer, args, useColor)
+}
